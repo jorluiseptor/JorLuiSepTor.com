235b208298aa5dff91045c2cbf5885fc1d8bac30
Jorge Sepulveda <jorluiseptor@gmail.com>
2013-11-22 20:36:27 -0500

    - Add bootstrap. - Add Glyphicons. - Add head meta tags.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# This is my README
+# jorluiseptor.com
+# This is to post my web development work, RSS from 8PEX, photos and other info.
---
+++ web/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+  <head>
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge"/>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="description" content="Jorge is a photographer, information technology specialist, self-proclaimed computer geek, and fan of the arts. Some of his passions are technology, visual arts, music, programming, Internet, online gaming, philosophy, science, and interesting." /><meta property="og:title" content="Jorge Sepúlveda" />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="http://jorluiseptor.com" />
+	<meta property="og:description" content="Jorge is a photographer, information technology specialist, self-proclaimed computer geek, and fan of the arts. Some of his passions are technology, visual arts, music, programming, Internet, online gaming, philosophy, science, and interesting." />
+    <title>Jorge Sepúlveda</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet/less" type="text/css" href="css/style.less" />
+  </head>
+  <body>
+	<div class="container">
+	
+		<div class="row">
+			<div class="col-md-3" ata-spy="affix" data-offset-top="200">
+				<h1>Jorge Sepúlveda</h1>
+				<ul class="nav nav-pills nav-stacked">
+				  <li><a href="#">Bio</a></li>
+				</ul>
+			</div>
+			<div class="col-sm-9">
+				<section id="about">
+					<p>
+					I'm a perfect person in beta version. Some bugs may be occur. I'm sometimes human. 
+					</p>
+					<p>
+					I'm an information technology specialist, photographer, self-proclaimed computer geek, professor of computers, and fan of the arts. Some of my passions are technology, visual arts, music, programming, Internet, online gaming, philosophy, science, and anything else thought-provoking. 
+					</p>
+					<p>
+					I sing with Cantigas and manage their website, I created and write for 8PEX, Roadzer, I like to paint a little.
+					</p>
+					<p>
+					Jorge </br>
+					jorluiseptor (at) gmail (dot) com
+					</p>
+				</section>
+			</div>
+
+		</div>
+	</div>
+
+	
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="//code.jquery.com/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+	<script src="js/less.js" type="text/javascript"></script> 
+  </body>
+</html>
