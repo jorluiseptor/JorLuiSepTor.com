c77a8396d25ee7673aff52dbbecf2ea2910367d7
Jorge Sepulveda <jorluiseptor@gmail.com>
2013-11-23 20:42:49 -0500

    - Fix accents not displaying correctly. - Add fancybox library (needs more work). - Add screenshots of sobeorganic.com - Add screenshots of erizoeditorial.com - Merge all jquery libs into one file.

--- web/index.php
+++ web/index.php
@@ -1,15 +1,15 @@
-<!DOCTYPE html>
+ï»¿<!DOCTYPE html>
 <html lang="en">
   <head>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=Edge"/>
 	
 	<meta name="description" content="Jorge is a photographer, information technology specialist, self-proclaimed computer geek, and fan of the arts. Some of his passions are technology, visual arts, music, programming, Internet, online gaming, philosophy, science, and interesting." />
-	<meta property="og:title" content="Jorge Sepúlveda" />
+	<meta property="og:title" content="Jorge SepÃºlveda" />
 	<meta property="og:type" content="website" />
 	<meta property="og:url" content="http://jorluiseptor.com" />
 	<meta property="og:description" content="Jorge is a photographer, information technology specialist, self-proclaimed computer geek, and fan of the arts. Some of his passions are technology, visual arts, music, programming, Internet, online gaming, philosophy, science, and interesting." />
-    <title>Jorge Sepúlveda</title>
+    <title>Jorge SepÃºlveda</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="css/style.css" />
   </head>
@@ -17,13 +17,17 @@
 	<div class="container">
 	
 		<div class="row">
-			<div class="col-md-3 nav-column" ata-spy="affix" data-offset-top="200">
-				<h1>Jorge Sepúlveda</h1>
-				<ul class="nav nav-pills nav-stacked">
-				  <li><a href="#bio" data-page="bio.php">Bio</a></li>
-				  <li><a href="#projects" data-page="projects.php">Projects</a></li>
-				  
-				</ul>
+			<div class="col-md-3"  ata-spy="affix" data-offset-top="200">
+				<div class="nav-column">
+					<h1>Jorge SepÃºlveda</h1>
+					<ul class="nav nav-pills nav-stacked">
+					  <li><a href="#bio" data-page="bio.php">Bio</a></li>
+					  <li><a href="#projects" data-page="projects.php">Projects</a>
+
+					  </li>
+					  
+					</ul>
+				</div>
 			</div>
 			<div class="col-md-6 col-md-offset-3">
 				<div class="content">
@@ -38,8 +42,7 @@
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="//code.jquery.com/jquery.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/jquery.backstretch.min.js"></script>
+    <script src="js/libs.js"></script>
 	<script src="js/main.js" type="text/javascript"></script> 
   </body>
 </html>
