a2cec6e6cafee5be136f8384a27d0fe16f1cd038
Jorge Sepulveda <jorluiseptor@gmail.com>
2016-11-11 08:34:56 -0500

    Add nav bar.

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -8,13 +8,12 @@ import {BrowserModule} from "@angular/platform-browser";
 import {HttpModule} from "@angular/http";
 import {AboutComponent} from './about/about.component';
 import {HomeComponent} from './home/home.component';
-import {RepoBrowserComponent} from './github/repo-browser/repo-browser.component';
 import {RepoListComponent} from './github/repo-list/repo-list.component';
 import {RepoDetailComponent} from './github/repo-detail/repo-detail.component';
 import {LocationStrategy, HashLocationStrategy} from '@angular/common';
 
 @NgModule({
-  declarations: [AppComponent, AboutComponent, RepoBrowserComponent, RepoListComponent, RepoDetailComponent, HomeComponent],
+  declarations: [AppComponent, AboutComponent, RepoListComponent, RepoDetailComponent, HomeComponent],
   imports     : [BrowserModule, FormsModule, HttpModule, RouterModule.forRoot(rootRouterConfig)],
   providers   : [GithubService, {provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap   : [AppComponent]
