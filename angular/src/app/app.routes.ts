12a85077a536c59441dba505db9cf1f1bfdc8582
Jorge Sepulveda <jorluiseptor@gmail.com>
2016-11-11 18:43:24 -0500

    WIP

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import {Routes} from '@angular/router';
 import {AboutComponent} from './about/about.component';
 import {HomeComponent} from './home/home.component';
-import {RepoBrowserComponent} from './github/repo-browser/repo-browser.component';
 import {RepoListComponent} from './github/repo-list/repo-list.component';
 import {RepoDetailComponent} from './github/repo-detail/repo-detail.component';
 
