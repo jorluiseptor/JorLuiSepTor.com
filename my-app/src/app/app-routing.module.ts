3bac80e7c3ceb90933be65ec85aa98975eba034a
Jorge Sepulveda <jorluiseptor@gmail.com>
2017-12-10 04:53:28 -0500

    Add saffolding for Home and About components.

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+//for each component that needs to be in the rout, import it here
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+
+//Here you specify the routes and any route parameters if needed.
+const routes: Routes = [
+  {
+    path:'',
+    component: HomeComponent
+  },
+  {
+    path:'about',
+    component: AboutComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
