42e6d633b517b7ea9057b7406aac82e86010d5f5
Jorge Sepulveda <jorluiseptor@gmail.com>
2019-03-27 16:25:45 -0400

    WIP

---
+++ index.php
@@ -0,0 +1,59 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+  <head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge"/>
+	<link rel="icon" href="img/favicon.png" sizes="16x16" type="image/png">
+	<link rel="sitemap" type="application/xml" title="Sitemap" href="/sitemap.xml">
+	<meta name="description" content="Jorge is a photographer, information technology specialist, self-proclaimed computer geek, and fan of the arts. Some of his passions are technology, visual arts, music, programming, Internet, online gaming, philosophy, science, and interesting." />
+	<meta property="og:title" content="Jorge Sepúlveda" />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="http://jorluiseptor.com" />
+	<meta property="og:description" content="Jorge is a photographer, information technology specialist, self-proclaimed computer geek, and fan of the arts. Some of his passions are technology, visual arts, music, programming, Internet, online gaming, philosophy, science, and interesting." />
+    <title>Jorge Sepúlveda</title>
+    
+	<link rel="stylesheet" href="css/style.css" />
+  </head>
+  <body>
+  
+	<div class="container">	
+		<div class="row">
+			<div class="col-md-3"  ata-spy="affix" data-offset-top="200">
+				<div class="nav-column">
+					<h1>Jorge Sepúlveda</h1>
+					<ul class="nav nav-pills nav-stacked">
+					  <li><a href="#bio" data-page="bio.php">Bio</a></li>
+					  <li><a href="#projects" data-page="projects.php">Projects</a></li>
+					  <li><a href="#photography" data-page="photography.php">Photography</a></li>
+					  <li><a href="#videos" data-page="videos.php">Videos</a></li>
+					  <li><a href="#cv" data-page="cv.php">Curriculum Vitae</a></li>
+					  <li><a href="#contact" data-page="contact.php">Contact Me</a></li>					  
+					</ul>
+				</div>
+			</div>
+			<div class="col-md-6 col-md-offset-3">
+				<div class="content-placeholder"></div>
+			</div>
+		</div>
+	</div>
+
+	<!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="//code.jquery.com/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/libs.js"></script>
+	<script src="js/main.js" type="text/javascript"></script> 
+  </body>
+</html>
