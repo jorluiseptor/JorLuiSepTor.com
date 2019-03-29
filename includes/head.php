04c445e20e58a37fd6c14ed42c99dd49a69be26c
Jorge Sepulveda <jorluiseptor@gmail.com>
2019-03-28 21:13:00 -0400

    Includes directory

---
+++ includes/head.php
@@ -0,0 +1,17 @@
+	<!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		<link rel="stylesheet" href="/css/style.css" />
+
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge"/>
+	<link rel="icon" href="img/favicon.png" sizes="16x16" type="image/png">
+	<link rel="sitemap" type="application/xml" title="Sitemap" href="/sitemap.xml">
+	<meta name="description" content="Jorge is a photographer, information technology specialist, self-proclaimed computer geek, and fan of the arts. Some of his passions are technology, visual arts, music, programming, Internet, online gaming, philosophy, science, and interesting." />
+	<meta property="og:title" content="Jorge Sepúlveda" />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="http://jorluiseptor.com" />
+	<meta property="og:description" content="Jorge is a photographer, information technology specialist, self-proclaimed computer geek, and fan of the arts. Some of his passions are technology, visual arts, music, programming, Internet, online gaming, philosophy, science, and interesting." />
+    <title>Jorge Sepúlveda</title>
--- index.php
+++ index.php
@@ -1,24 +1,8 @@
 ﻿<!DOCTYPE html>
 <html lang="en">
   <head>
-	<!-- Required meta tags -->
-	<meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-		<link rel="stylesheet" href="/css/style.css" />
-
-	<meta http-equiv="X-UA-Compatible" content="IE=Edge"/>
-	<link rel="icon" href="img/favicon.png" sizes="16x16" type="image/png">
-	<link rel="sitemap" type="application/xml" title="Sitemap" href="/sitemap.xml">
-	<meta name="description" content="Jorge is a photographer, information technology specialist, self-proclaimed computer geek, and fan of the arts. Some of his passions are technology, visual arts, music, programming, Internet, online gaming, philosophy, science, and interesting." />
-	<meta property="og:title" content="Jorge Sepúlveda" />
-	<meta property="og:type" content="website" />
-	<meta property="og:url" content="http://jorluiseptor.com" />
-	<meta property="og:description" content="Jorge is a photographer, information technology specialist, self-proclaimed computer geek, and fan of the arts. Some of his passions are technology, visual arts, music, programming, Internet, online gaming, philosophy, science, and interesting." />
-    <title>Jorge Sepúlveda</title>
-    
+    <?php include('includes/head.php'); ?>
 
   </head>
   <body>
@@ -45,14 +29,6 @@
 		</div>
 	</div>
 
-	<!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-		<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="//code.jquery.com/jquery.js"></script>
-	<script src="js/main.js" type="text/javascript"></script> 
+	<?php include('includes/footer.php'); ?>
   </body>
 </html>
